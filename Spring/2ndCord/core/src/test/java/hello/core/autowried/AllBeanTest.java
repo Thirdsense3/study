5b36d39c6ee8c565ab415d6b3d21cdafe26a4b22
Thirdsense <myu1231@naver.com>
2022-10-23 22:52:55 +0900

    2022.10.21 19:03 금요일 스프링

--- Spring/2ndCord/core/src/test/java/hello/core/autowried/AllBeanTest.java
+++ Spring/2ndCord/core/src/test/java/hello/core/autowried/AllBeanTest.java
@@ -2,7 +2,10 @@ package hello.core.autowried;
 
 import hello.core.AutoAppConfig;
 import hello.core.discount.DiscountPolicy;
+import hello.core.member.Grade;
+import hello.core.member.Member;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -12,17 +15,27 @@ import java.util.Map;
 public class AllBeanTest {
 
     @Test
-    void findAllBean(){
+    void findAllBean() {
         ApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class, DiscountService.class);
+
+        DiscountService discountService = ac.getBean(DiscountService.class);
+        Member member = new Member(1L, "userA", Grade.VIP);
+        int discount =  discountService.discount(member,10000,"fixDiscountPolicy");
     }
+
 
     static class DiscountService {
         private final Map<String, DiscountPolicy> policyMap;
         private final List<DiscountPolicy> policies;
 
+        @Autowired
         public DiscountService(Map<String, DiscountPolicy> policyMap, List<DiscountPolicy> policies) {
             this.policyMap = policyMap;
             this.policies = policies;
+
+            System.out.println("policyMap = " + policyMap);
+            System.out.println("policies = " + policies);
         }
+
     }
 }
