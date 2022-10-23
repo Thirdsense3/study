03983e6aac8e57bf24f9b2d3ab4d72c859010bbe
Thirdsense <myu1231@naver.com>
2022-10-23 23:15:52 +0900

    2022.10.20 23:48 목요일 스프링

--- Spring/2ndCord/core/src/test/java/hello/core/autowried/AllBeanTest.java
+++ Spring/2ndCord/core/src/test/java/hello/core/autowried/AllBeanTest.java
@@ -4,6 +4,7 @@ import hello.core.AutoAppConfig;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Grade;
 import hello.core.member.Member;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
@@ -11,6 +12,8 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 import java.util.List;
 import java.util.Map;
+
+import static org.assertj.core.api.Assertions.*;
 
 public class AllBeanTest {
 
@@ -20,7 +23,14 @@ public class AllBeanTest {
 
         DiscountService discountService = ac.getBean(DiscountService.class);
         Member member = new Member(1L, "userA", Grade.VIP);
-        int discount =  discountService.discount(member,10000,"fixDiscountPolicy");
+        int discountPrice =  discountService.discount(member,10000,"fixDiscountPolicy");
+
+        assertThat(discountService).isInstanceOf(DiscountService.class);
+        assertThat(discountPrice).isEqualTo(1000);
+
+        int rateDiscountPrice = discountService.discount(member, 20000, "rateDiscountPolicy");
+        assertThat(rateDiscountPrice).isEqualTo(2000);
+
     }
 
 
@@ -28,7 +38,6 @@ public class AllBeanTest {
         private final Map<String, DiscountPolicy> policyMap;
         private final List<DiscountPolicy> policies;
 
-        @Autowired
         public DiscountService(Map<String, DiscountPolicy> policyMap, List<DiscountPolicy> policies) {
             this.policyMap = policyMap;
             this.policies = policies;
@@ -37,5 +46,9 @@ public class AllBeanTest {
             System.out.println("policies = " + policies);
         }
 
+        public int discount(Member member, int price, String discountCode) {
+            DiscountPolicy discountPolicy = policyMap.get(discountCode);
+            return discountPolicy.discount(member,price);
+        }
     }
 }
