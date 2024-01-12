40483400fac5791469062ad9ebcfcdb3ebba8f90
Thirdsense <myu1231@naver.com>
2024-01-12 22:58:17 +0900

    2024.01.12 "스프링 MVC 1편" - 세션 4. 단순하고 실용적인 컨트롤러 - v4

--- Spring/servlet/servlet/src/main/java/hello/servlet/web/frontcontroller/MyView.java
+++ Spring/servlet/servlet/src/main/java/hello/servlet/web/frontcontroller/MyView.java
@@ -6,6 +6,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.Map;
 
 public class MyView {
     private String viewPath;
@@ -16,6 +17,14 @@ public class MyView {
 
     public void render(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         RequestDispatcher requestDispatcher = request.getRequestDispatcher(viewPath);
+        requestDispatcher.forward(request, response);
+    }
+
+    public void render(Map<String, Object> model, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        model.forEach(request::setAttribute);
+
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher(viewPath);
+
         requestDispatcher.forward(request, response);
     }
 }
