079a661b6d9d159e663558442346f85da4d7ba26
Thirdsense <myu1231@naver.com>
2024-02-06 23:15:58 +0900

    2024.02.06 "스프링 DB 1편" - JDBC 개발 - 조회,수정,삭제

--- Spring/스프링 DB/jdbc/jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
+++ Spring/스프링 DB/jdbc/jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -1,19 +1,50 @@
 package hello.jdbc.repository;
 
 import hello.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
+import java.util.NoSuchElementException;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
 
+@Slf4j
 class MemberRepositoryV0Test {
 
-    MemberRepositoryV0 repositoryV0 = new MemberRepositoryV0();
+    MemberRepositoryV0 repository = new MemberRepositoryV0();
 
     @Test
     void crud() throws SQLException {
-        Member member = new Member("memeberV0", 10000);
-        repositoryV0.save(member);
+        //save
+        Member member = new Member("memeberV10", 10000);
+        repository.save(member);
+
+        //findById
+        Member findMember = repository.findById(member.getMemberId());
+        log.info("findMember={}", findMember);
+
+        /**
+         * "==" 비교시 인스턴스는 다름.
+         */
+        log.info("member == findMember {}", member == findMember);
+
+        /**
+         * equals로 비교시에는 값비교라서 같음. -> @Data에 있는 EqualsandHashCode기능.
+         */
+        log.info("member equals findMember {}", member.equals(findMember));
+
+        assertThat(findMember).isEqualTo(member);
+
+        //update: money:1000 -> 20000
+        repository.update(member.getMemberId(), 20000);
+        Member updateMember = repository.findById(member.getMemberId());
+        assertThat(updateMember.getMoney()).isEqualTo(20000);
+
+        //delete
+        repository.delete(member.getMemberId());
+        assertThatThrownBy(() -> repository.findById(member.getMemberId()))
+                .isInstanceOf(NoSuchElementException.class);
     }
 }
